50ae115187f00f4f6f3c9cd7a3cd4cdcfb3f8c3c
rubui <32724161+rubui@users.noreply.github.com>
2020-02-13 17:20:01 -0800

    Initial Update
    Added basic project, education, and experience section

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var i = 0;
+var txt = ' Hello World!';
+var speed = 50;
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("snippet1").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+$(document).ready(function(){
+  $("#dropdownbtn").click(function(){
+    $("#dropdown").slideToggle("fast");
+  });
+  
+  if($("#dropdown").is(":visible")){            $('.down').addClass('up').removeClass('down');
+  } else{
+    $('.up').addClass('down').removeClass('up');
+  
+  }
+  
+  //typeWriter();
+});
